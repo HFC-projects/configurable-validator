1ddd314ba14d66124e5157bb8a8eab97f644be7c
Itay Waisman <itay.waisman@gmail.com>
2020-06-11 12:41:00 +0300

    added return type for the validator

--- packages/core/src/validator.ts
+++ packages/core/src/validator.ts
@@ -1,11 +1,11 @@
-import { ValidationOptions, IValidator, ConstraintsMapping } from "./interfaces";
+import { ValidationOptions, IValidator, ConstraintsMapping, ValidationResult } from "./interfaces";
 
 export class Validator implements IValidator {
 
 
-    public validate<T>(objectsToValidate: T[], constraints: ConstraintsMapping, options?: ValidationOptions, data?: any)
-    public validate<T>(objectsToValidate: T[], constraints: ConstraintsMapping, data?: any)
-    public validate<T>(objectsToValidate: T[], constraints: ConstraintsMapping, options?: ValidationOptions) {
-
+    public validate<T>(objectsToValidate: T[], constraints: ConstraintsMapping, options?: ValidationOptions, data?: any) : ValidationResult
+    public validate<T>(objectsToValidate: T[], constraints: ConstraintsMapping, data?: any) : ValidationResult
+    public validate<T>(objectsToValidate: T[], constraints: ConstraintsMapping, options?: ValidationOptions) : ValidationResult {
+        return undefined;
     }
 }
